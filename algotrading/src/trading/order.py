8a2435af6e733d292a76c6984f1f4e15e17edc4e
Sean Meehan <seanmeehan@192-168-1-117.tpgi.com.au>
2024-06-13 21:13:25 +1000

    brought across old methods

--- algotrading/src/trading/order.py
+++ algotrading/src/trading/order.py
@@ -1,4 +1,5 @@
 import logging
+from ibapi.order import Order
 
 class OrderManager:
     def __init__(self, config: dict, pipeline: dict):
@@ -6,3 +7,68 @@ class OrderManager:
 
         self.config = config
         self.pipeline = pipeline
+
+
+    def checkAction(self, action, active_pos) -> bool:
+        if action != 'NONE' and action != active_pos and '_PEND' not in active_pos and '_FILL' not in active_pos:
+            return True
+        else:
+            return False
+        
+
+    def calcOrderSpec(self, balance, units, action, activePos, price):
+        
+        #TODO Move multiplier to pipeline config
+        adj_balance = balance * 0.9
+        unit_amt = round(adj_balance / price, 0)
+        mod_string = activePos + action
+        
+        if units == 0:
+            units = unit_amt
+        
+        self.logger.info(f'Figures: adj_balance {adj_balance}, unit_amt {unit_amt}, mod_string {mod_string}')
+        
+        action_dict = {'NONEBUY': ['BUY', unit_amt],
+                       'SELLBUY': ['NONE', abs(units)],
+                       'NONESELL': ['SELL', unit_amt],
+                       'BUYSELL': ['NONE', abs(units)]}
+        
+        self.logger.info(f'Order spec: {action_dict[mod_string]}')
+        
+        return action_dict[mod_string]
+
+
+    def executeOrder(self, orderAction, units) -> tuple:
+        
+        order = Order()
+        order.action = orderAction
+        order.totalQuantity = units
+        order.orderType = 'MKT'
+        order.eTradeOnly = False
+        order.firmQuoteOnly = False
+        
+        self.logger.info(f'units: {units}')
+        
+        #TODO No init_oType in self
+        if active_pos == 'NONE' and orderAction not in self.init_oType:
+            pass
+        else:
+            active_pos = '{}_PEND'.format(orderAction)
+
+        return order, active_pos
+
+
+    def positionUnlock(self, active_position, balance_figure, balance_list, update_position) -> tuple:
+        
+        balance_list.append(balance_figure)
+        
+        if '_FILL' in active_position and len(balance_list) == 2:
+            return_pos = update_position
+            balance_list = []
+            
+        else:
+            return_pos = active_position
+            
+        self.logger.info(f'{return_pos}, {balance_list}')
+        
+        return return_pos, balance_list
--- algotrading/src/trading/trading.py
+++ algotrading/src/trading/trading.py
@@ -2,11 +2,13 @@ import logging
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract
+from threading import Timer
 from ..models.predict import Predict
 from .order import OrderManager
 
 class Trading(EWrapper, EClient):
-    BASE_SECONDS = 20
+    BASE_SECONDS = 60
+    CURRENT_POS_LIST = []
 
     def __init__(self, config: dict, pipeline: dict):
         EClient.__init__(self, self)
@@ -19,24 +21,111 @@ class Trading(EWrapper, EClient):
         self.predict = Predict(self.config, self.pipeline)
         self.order = OrderManager(self.config, self.pipeline)
 
+        contract_info = self.pipeline['pipeline']['contract_info']
+        self.contract = Contract()
+        self.contract.symbol = contract_info['symbol']
+        self.contract.secType = contract_info['secType']
+        self.contract.exchange = contract_info['exchange']
+        self.contract.currency = contract_info['currency']
+        self.contract.primaryExchange = contract_info['primaryExchange']
+
+        self.account = self.config['account_number']
+
         self.timer = self.setTimer()
 
 
-    def error(self, reqId, errorCode, errorString, advancedOrderRejectJson) -> None:
+    def error(self, reqId, errorCode, errorString) -> None:
         self.logger.info(f'Error: {reqId}, {errorCode}, {errorString}')
 
 
     def connect(self, ip_address, port, client_id):
         super().connect(ip_address, port, client_id)
 
+        #
+        self.logger.info('Trading connection request sent')
+
+        self.start()
+
 
     def nextValidId(self, orderId: int) -> None:
         super().nextValidId(orderId)
         self.nextValidOrderId = orderId
 
+        #
+        self.logger.info('Starting Trading connection')
+
+
+    def nextOrderId(self):
+        self.oid = self.nextValidOrderId
+        self.nextValidOrderId += 1
+        return self.oid
+
     
     def setTimer(self) -> int:
         return self.BASE_SECONDS
+    
+
+    # Account data updates
+    def updateAccountValue(self, key: str, val: str, currency: str, accountName: str) -> None:
+        self.logger.info(f'Account update: {key} {val} {currency} {accountName}')
+
+        if key == 'CashBalance' and currency == self.contract.currency:
+            self.cashbalance = float(val)
+
+            self.logger.info(f'cashbalance: {self.cashbalance}')
+
+            if '_FILL' in self.active_pos:
+                self.active_pos, self.current_pos_list = self.order.positionUnlock(self.active_pos, self.cashbalance, self.CURRENT_POS_LIST, self.order_spec[0])
+
+    
+    def updatePortfolio(self, contract: Contract, position: float, marketPrice: float, marketValue: float, averageCost: float, unrealizedPNL: float, realizedPNL: float, accountName: str) -> None:
+        self.logger.info(f'Portfolio update: {contract} {position} {marketPrice} {marketValue} {averageCost} {unrealizedPNL} {realizedPNL} {accountName}')
+
+        if contract.symbol == self.contract.symbol:
+            self.openunits = position
+            
+            self.logger.info(f'openunits: {self.openunits}')
+            
+            if '_FILL' in self.active_pos:
+                self.active_pos, self.current_pos_list = self.order.positionUnlock(self.active_pos, self.openunits, self.CURRENT_POS_LIST, self.order_spec[0])     
+
+    
+    def updateAccountTime(self, timeStamp: str) -> None:
+        self.logger.info(f'Account time update: {timeStamp}')
+
+
+    def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
+        self.logger.info(f'OrderStatus. Id: {orderId}, Status: {status}, {filled}, {remaining}, {avgFillPrice}, {permId}, {parentId}, {lastFillPrice}, {clientId}, {whyHeld}, {mktCapPrice}')
+        
+        if (status == 'PreSubmitted' or status == 'Submitted') and filled == 0:
+            self.timing = Timer(15, self.stopCancel, args=[self.oid])
+            self.timing.start()
+            self.timer = True
+            
+        elif (status == 'PreSubmitted' or status == 'Submitted') and filled > 0:
+            
+            self.last_price = avgFillPrice
+            self.last_pos = self.order_spec[0]
+            self.active_pos = '{}_PEND'.format(self.temp_action)
+            
+            self.logger.info('PART, {self.active_pos}, {self.last_pos}, {self.last_price}')            
+            
+        elif status == 'Filled':
+            self.timing.cancel()
+            self.timer = False
+            
+            self.last_price = avgFillPrice
+            self.last_pos = self.order_spec[0]
+            self.active_pos = '{}_FILL'.format(self.temp_action)
+            
+            self.logger.info('FILL, {self.active_pos}, {self.last_pos}, {self.last_price}')  
+
+
+    def stopCancel(self, orderId):
+        if '_PEND' in self.active_pos:
+            self.cancelOrder(orderId)
+            self.timer = False
+            self.active_pos = self.last_pos
         
 
     def trading_algorithm(self, state: dict) -> None:
@@ -46,11 +135,39 @@ class Trading(EWrapper, EClient):
 
         self.logger.info(f'action: {action_int}')
 
-        return action_int
+        take_action = self.order.checkAction(action_int, self.active_pos)
+
+        if take_action:
+            #TODO Does temp action need to be parsed along?
+            self.temp_action = action_int
+
+            #TODO No last_price yet
+            self.order_spec = self.order.calcOrderSpec(self.cashbalance, self.openunits, action_int, self.active_pos, 
+                                                       self.last_price)
+
+            #TODO Possibly execute within Trading class
+            order, self.active_pos = self.order.executeOrder(self.contract, self.temp_action, self.order_spec[1])
+            
+            #TODO This should be a separate method along with above
+            self.placeOrder(self.nextOrderId(), self.contract, order)
+        else:
+            self.logger.info('No action taken')
+
+        return None
+    
+
+    def start(self) -> None:
+
+        #
+        self.logger.info('Calling reqAccountUpdates')
+        
+        self.reqAccountUpdates(True, '')
     
     
     def stop(self) -> None:
         self.logger.info('Trading connection closed')
+
+        self.reqAccountUpdates(False, '')
         
         self.done = True
         self.disconnect()
